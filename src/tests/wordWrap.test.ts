4031376953f15ae9ebccd27e32d6f99942ddc290
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-22 09:37:00 +0200

    Red: returns a text where every single line fits within maximun range allowed

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -1,5 +1,23 @@
+/*
+Lista de ejemplos a testear:
+- wordWrap('',5) ⇒ ''
+- wordWrap('hello',5) ⇒ 'hello'
+- wordWrap('longword',4) ⇒ 'long\nword'
+- wordWrap('reallylongword',4) ⇒ 'real\nlylo\nngwo\nrd'
+- wordWrap('abc def',4) ⇒ 'abc\ndef' 
+- wordWrap('abc def ghi',4) ⇒ 'abc\ndef\nghi'
+- wordWrap(' abcdf',4) ⇒ '\nabcd\nf'
+- wordWrap(null,5) ⇒ ''
+- wordWrap('hello',-5) ⇒ throw exception
+ */
 describe("The WordWrap function", ()=>{
-    it("", ()=>{
-        expect(true).toBe(true);
+    it("returns a text where every single line fits within maximun range allowed", ()=>{
+        expect(wordWrap('hello', 5)).toBe('hello');
     });
 });
+
+//Secuencia de prioridad de transformación: 1 >> De no haber código a devolver nulo
+function wordWrap(text: string, columnWidth: number): any {
+    return undefined;
+}
+
