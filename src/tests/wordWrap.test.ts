0bcc9990a6c08c55ba400316f7fa7007d49eaafe
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-22 09:40:50 +0200

    Red: returns a text where every single line fits within maximun range allowed

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -13,6 +13,7 @@ Lista de ejemplos a testear:
 describe("The WordWrap function", ()=>{
     it("returns a text where every single line fits within maximun range allowed", ()=>{
         expect(wordWrap('hello', 5)).toBe('hello');
+        expect(wordWrap('longword', 4)).toBe('long\nword');
     });
 });
 
